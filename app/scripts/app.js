8fdd75b199ad51a45a9e12722ee1abead7bc00ac
Tzook <Tzook@users.noreply.github.com>
2014-12-08 19:40:18 +0200

    Create app.js

---
+++ app/scripts/app.js
@@ -0,0 +1,28 @@
+/*===========================================================================
+** Main module of the project.
+** Includes the directives and the config.
+*===========================================================================*/
+(function(angular){
+	'use strict';
+	var myApp = angular.module('myApp', [
+		'ngRoute',
+		'controllers'
+	])
+	.directive('footer', function () { // replaces the normal <footer> element
+	    return {
+	    	restrict: 'E',
+		    templateUrl: 'partials/footer.html',
+		    controller: 'FooterController'
+	    };
+	})
+	.config(['$routeProvider', function ($routeProvider) {
+		$routeProvider
+		.when('/', {
+			templateUrl: 'partials/main.html',
+			controller: 'MainController'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+	}]);
+})(angular);
