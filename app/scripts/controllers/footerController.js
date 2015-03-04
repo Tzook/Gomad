b267f9418f505c3684b415fa60c0e3be5f8e1563
Tzook Shaked <Tzook10@gmail.com>
2015-03-04 08:44:06 +0200

    changed About to contain Ben

--- app/scripts/controllers/footerController.js
+++ app/scripts/controllers/footerController.js
@@ -7,12 +7,11 @@
 	angular.module('footerController', [])
 	.controller('FooterController', ['$scope', function($scope) {
 		/* info about the developers */
-		$scope.about = '<p>Created by:<br>'
+		$scope.about = '<p>Web created by:<br>'
 					 + '<a target="_blank" href="https://www.linkedin.com/pub/tzook-shaked/a4/230/6a0">Tzook</a>'
 					 + ' & <a target="_blank" href="https://www.linkedin.com/pub/noam-elboim/a6/372/a">Noam</a><br>'
-					 + '<a target="_blank" href="mailto:tzook10@gmail.com">tzook10@gmail.com</a><br>'
-					 + '<a target="_blank" href="mailto:noam@mail.com">noam@mail.com</a><br>'
 					 + '<a target="_blank" href="https://github.com/Tzook/Gomad">Source</a><br>'
-					 + 'Music by:<br><a target="_blank" href="mailto:guyguyguy65@mail.com">Guy Benami</a></p>';
+					 + 'Games created by:<br><a target="_blank" href="mailto:benk0913@gmail.com">Ben Rokah</a><br>'
+					 + 'Music by:<br><a target="_blank" href="mailto:guyguyguy65g@mail.com">Guy Benami</a></p>';
 	}]);
 })(angular);
